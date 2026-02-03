e4cc91fa718b240793e8870a4dfe99bd57e7217b
Haroon Khalid <muhammadharoonkhalid786@gmail.com>
2026-02-03 08:55:29 +0500

    Update 1. Two Sum.py

--- Easy/1. Two Sum.py
+++ Easy/1. Two Sum.py
@@ -2,8 +2,8 @@ class Solution:
     @staticmethod
     def twoSum(num, targ):
         for i in range(len(num)):
-            for j in num:
-                if i + j == targ:
-                    print(f"[{num.index(i)} ,{num.index(j)}]")
-                    return [num.index(i) ,num.index(j)]
+            for j in range(i+1, len(num)):
+                if num[i] + num[j] == targ:
+                    print(f"[{i} ,{j}]")
+                    return [i, j]
 o = Solution().twoSum([3,3],6)
