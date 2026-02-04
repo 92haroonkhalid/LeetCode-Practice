d4dff3d352faf19810d3c232a30adc4961a03a9d
Haroon Khalid <muhammadharoonkhalid786@gmail.com>
2026-02-04 14:25:02 +0500

    Implement addStrings method to sum two strings

---
+++ Easy/415. Add Strings.py
@@ -0,0 +1,9 @@
+class Solution:
+    def addStrings(self,a,b):
+        x = int(a)
+        y = int(b)
+        z = x+y
+        z = str(z)
+        return z
+o = Solution()
+o.addStrings("123","111")
